9bdf47a53004bf80ab33486144e904b574b86225
Yavor Yankov <yavakaa@gmail.com>
2019-02-17 16:01:18 +0000

    project files

---
+++ TriggerTheBomb/TriggerTheBomb/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TriggerTheBomb
+{
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random()
+            .Next(1, 4)
+            .ToString();
+        int scores = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded", "Game Over", "Retry");
+                bomb = new Random()
+                            .Next(1, 4)
+                            .ToString();
+                scores = 0;
+            }
+            else
+            {
+                scores++;
+                await DisplayAlert("Bomb Defused!", "Scores: " + scores, "Continue");
+                bomb = new Random()
+                            .Next(1, 4)
+                            .ToString();
+
+            }
+        }
+    }
+}
